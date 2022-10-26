ef65ed961b3547d34cc1efc56e5272b17e1f3b36
wspirrat <dawidnajdz@gmail.com>
2022-10-26 16:44:13 +0200

    swt u[date

--- swt/swt.go
+++ swt/swt.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
@@ -33,6 +34,7 @@ func NewEncodeKey() ENCODE_KEY {
 func AutoConfig() SWT_CONFIG {
 	host, err := os.Hostname()
 	if err != nil {
+		fmt.Println(err)
 		host = "default-unsecure please change username due to security reason"
 	}
 
@@ -57,24 +59,27 @@ type SWT struct {
 	Expire  time.Time
 }
 
-func EncodeSWT(values interface{}) (Payload string) {
+func EncodeSWT(value interface{}) (Payload string) {
 	now := time.Now()
 	init := SWT{
 		Typ:     "swt",
-		Payload: values,
+		Payload: value,
 		Expire:  now.Add(EXPIRE_TIME),
 	}
 	// p = payload
 	p := new(bytes.Buffer)
 
+	gob.Register(value)
 	enc := gob.NewEncoder(p)
 	err := enc.Encode(init)
 	if err != nil {
+		fmt.Println(err)
 		return "encode error"
 	}
 
 	psec, err := encrypt(p.String())
 	if err != nil {
+		fmt.Println(err)
 		return "encrypt error"
 	}
 	return psec
@@ -84,12 +89,14 @@ func DecodeSWT(Payload string) SWT {
 	var swt_cargo SWT
 	punsec, err := decrypt(Payload)
 	if err != nil {
+		fmt.Println(err)
 		return swt_cargo
 	}
 
 	pbytes := bytes.NewBufferString(punsec)
 	decoder := gob.NewDecoder(pbytes)
 	if err = decoder.Decode(&swt_cargo); err != nil {
+		fmt.Println(err)
 		return SWT{}
 	}
 
@@ -98,4 +105,8 @@ func DecodeSWT(Payload string) SWT {
 		return SWT{}
 	}
 	return swt_cargo
+}
+
+func (token SWT) IsPayloadNil() bool {
+	return token.Payload == nil
 }
