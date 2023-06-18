e2e2f3305743ea3ce384fa03cf4ac6d8c0727591
dwnajdz <dawidnajdz@gmail.com>
2023-06-18 22:55:06 +0200

    gif add

--- examples/login/login.go
+++ examples/login/login.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	swt.EXPIRE_TIME = time.Minute * 1
+	swt.EXPIRE_TIME = time.Second * 2
 	http.HandleFunc("/", home)
 	http.HandleFunc("/login", login)
 	log.Println("listening on http://localhost:80")
@@ -19,6 +19,7 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	safeToken := r.URL.Query().Get("token")
 	token := swt.DecodeSWT(safeToken)
 	if !token.IsPayloadNil() {
@@ -27,10 +28,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 		if payload["isLogged"].(bool) {
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			fmt.Fprintf(w, "Your username is: %s", payload["username"].(string))
-		} else {
-			fmt.Fprintf(w, "You dont have access to this website.")
+			fmt.Fprintf(w, `username: %s <br> password: %s <br> isLogged: %t
+				<br><br>
+				This token is only valid for 2 seconds, refresh it and it will be not valid.`,
+				payload["username"].(string), payload["password"].(string), payload["isLogged"].(bool))
 		}
+	} else {
+		fmt.Fprintf(w, "%s", `You dont have access to this website. go to <a href="/login">login page</a>`)
 	}
 }
 
@@ -38,10 +42,11 @@ func login(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		m := make(map[string]interface{})
 		m["username"] = r.FormValue("uname")
+		m["password"] = r.FormValue("psw")
 		m["isLogged"] = true
 		swt_cargo := swt.EncodeSWTcustom(m)
 
-		http.Redirect(w, r, fmt.Sprintf("http://localhost:80?token=%s", swt_cargo), 301)
+		http.Redirect(w, r, fmt.Sprintf("http://localhost:80?token=%s", swt_cargo), http.StatusMovedPermanently)
 	}
 
 	tmpl := template.Must(template.ParseFiles("login.html"))
